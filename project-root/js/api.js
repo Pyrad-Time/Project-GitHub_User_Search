36b4b70ca389c8bfcc11fe9896d3996852c050b2
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-07 23:53:38 -0300

    feat(api): add async fetch with error handling

--- project-root/js/api.js
+++ project-root/js/api.js
@@ -1,1 +1,19 @@
 // just do fetch
+import { state } from "./state.js"
+
+export async function getUser(userName) {
+
+    try {
+        const response = await fetch (`https://api.github.com/users/${userName}`)
+
+        if(!response.ok) 
+            throw new Error("User not found")
+
+        const data = await response.json()
+        
+        return data
+
+    } catch(error) {
+        return state.error = error
+    }   
+}
