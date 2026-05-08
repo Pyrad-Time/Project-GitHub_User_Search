efb3b559214a5b5ba20eb41db05b44a200a5c9f6
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-08 09:23:20 -0300

    feat(core): add user search input handling

--- project-root/js/main.js
+++ project-root/js/main.js
@@ -7,22 +7,31 @@ update state
 import { getUser } from "./api.js";
 import { state } from "./state.js";
 
+const input = document.getElementById("search-input")
+const searchBtn = document.getElementById("search-button")
+
 async function load(userName) {
     try {
     const data = await getUser(userName)
 
-    state.loading = !true
+    state.loading = false
     state.user = data.login
     state.profileImg = data.avatar_url
 
     } catch(error) {
         state.error = error.message
 
-        if(error.message === "Not Found") 
+        if(error.message === "User not found") 
             alert('User not found')
 
         alert(state.error)
     }    
 }
 
-load("pyrad-tme")
+searchBtn.addEventListener("click", () => {
+    state.inputValue = input.value
+
+    load(state.inputValue)
+})
+
+
