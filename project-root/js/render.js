89f0675c4cafc3510379e0447f724f2f95fce8fd
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-08 09:54:30 -0300

    feat(core): implement render profile UI from state

--- project-root/js/render.js
+++ project-root/js/render.js
@@ -1,1 +1,39 @@
 // Show ui informations
+import { state } from "./state.js";
+const container = document.getElementById("search-result")
+
+function renderProfile () {
+    const userName = document.createElement("p")
+    userName.textContent = state.user
+
+    const userProfileImg = document.createElement("img")
+    userProfileImg.src = state.profileImg
+
+
+    container.append(userName, userProfileImg)
+}
+
+function handleLoading() {
+    
+}
+
+function handleError() {
+
+}
+
+export function render() {
+    container.textContent = ""
+
+    if(state.loading) {
+        handleLoading()
+        return
+    }
+        
+
+    if(state.error) {
+        handleError()
+        return
+    }
+        
+    renderProfile()
+}
