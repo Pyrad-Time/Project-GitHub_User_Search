75b6831b878ab04a74ffa726dd7459ef693e2eb7
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-08 09:04:45 -0300

    feat(core): update state with API response and handle errors

--- project-root/js/main.js
+++ project-root/js/main.js
@@ -3,3 +3,26 @@ control events,
 call api, 
 update state
  */
+
+import { getUser } from "./api.js";
+import { state } from "./state.js";
+
+async function load(userName) {
+    try {
+    const data = await getUser(userName)
+
+    state.loading = !true
+    state.user = data.login
+    state.profileImg = data.avatar_url
+
+    } catch(error) {
+        state.error = error.message
+
+        if(error.message === "Not Found") 
+            alert('User not found')
+
+        alert(state.error)
+    }    
+}
+
+load("pyrad-tme")
