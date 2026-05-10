87741ea4b87fe2fd3ad3d1e54311658f9c771207
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-10 20:01:58 -0300

    feat(core): improve loading flow, error tranking and input validation

--- project-root/js/main.js
+++ project-root/js/main.js
@@ -5,12 +5,17 @@ update state
  */
 
 import { getUser } from "./api.js";
+import { render } from "./render.js";
 import { state } from "./state.js";
 
 const input = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-button")
 
+
 async function load(userName) {
+    state.loading = true
+    render()
+
     try {
     const data = await getUser(userName)
 
@@ -25,13 +30,33 @@ async function load(userName) {
             alert('User not found')
 
         alert(state.error)
+
+        state.errorLog += state.error
+        state.error = null
+
     }    
+
+    state.loading = false
+
+    render()
+
 }
 
-searchBtn.addEventListener("click", () => {
-    state.inputValue = input.value
+function btnSearch() {
 
-    load(state.inputValue)
+}
+
+searchBtn.addEventListener("click", async () => {
+
+    if(input.value) {
+        state.inputValue = input.value
+        await load(state.inputValue)
+    } else {
+        alert("Insira um nome válido.")
+    }
+        
+
+    console.log(state)
 })
 
 
