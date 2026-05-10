d49dfef627fa77104f85a6843e26f791ad6ab4a2
Pyrad-Time <marcos0fataak@gmail.com>
2026-05-10 19:56:00 -0300

    feat(state): add input, value, loading state and error tracking

--- project-root/js/state.js
+++ project-root/js/state.js
@@ -1,7 +1,9 @@
 // global storage
 export const state = {
+    inputValue: null,
     user: null,
-    loading: false,
+    loading: true,
     error: null,
+    errorLog: null,
     profileImg: null
 }
